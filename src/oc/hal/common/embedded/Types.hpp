f3e1b92f80392c4f86e2b3c48f3db26c736da368
petitechose-audio <contact@petitechose.audio>
2026-01-21 11:00:32 +0100

    refactor: update for oc::type namespace
    Adapt to framework breaking change: types moved to oc::type namespace.

--- src/oc/hal/common/embedded/Types.hpp
+++ src/oc/hal/common/embedded/Types.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
-#include <oc/types/Ids.hpp>
-#include <oc/types/Callbacks.hpp>
+#include <oc/type/Ids.hpp>
+#include <oc/type/Callbacks.hpp>
 
 namespace oc::hal::common::embedded {
 
 // Re-export IDs for convenience
-using oc::ButtonID;
-using oc::EncoderID;
+using oc::type::ButtonID;
+using oc::type::EncoderID;
 
 }  // namespace oc::hal::common::embedded
